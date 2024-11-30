bc21af98493bf5eb8644b8883d25da3cd587265b
Shaan Yadav <shaanyworkspace@gmail.com>
2024-11-30 23:43:44 +0530

    cc

---
+++ src/main/java/com/servlets/registerServlet.java
@@ -0,0 +1,22 @@
+package com.servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class registerServlet extends HttpServlet {
+
+    public registerServlet() {
+        super();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+    //generate dopost method
+   
+}
